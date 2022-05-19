94bc083de4c30ecb7665eb01c0d0f21551885113
Jihyun Kim <86540825+tamagoyakii@users.noreply.github.com>
2022-05-19 18:21:41 +0900

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -12,9 +12,9 @@ umem=$(free -m | grep Mem | awk '{print $3}')
 pmem=$(free -m | grep Mem | awk '{printf "%.2f", $3/$2*100}' | xargs)
 printf "#Memory Usage: ${umem}/${tmem}MB (${pmem}%%)\n"
 #5
-tdisk=$(df -m | grep LVMGroup | awk '{sum+=$2}END{print sum}')
-udisk=$(df -m | grep LVMGroup | awk '{sum+=$3}END{print sum}')
-pdisk=$(df -h | grep root | awk '{print $5}' | tr -d '%')
+tdisk=$(df -m | grep LVMGroup | awk '{sum+=$2}END{printf "%.2f", sum/1000}'
+udisk=$(df -m | grep LVMGroup | awk '{sum+=$3}END{printf "%.2f", sum/1000}'
+pdisk=$(df -m | grep LVMGroup | awk '{tdisk+=$2}{udisk+=$3}END{printf "%.2f", udisk/tdisk*100}')
 printf "#Disk Usage: ${udisk}/${tdisk}GB (${pdisk}%%)\n"
 #6
 printf "#CPU Load: $(top -bn1 | grep Cpu | awk '{print $2+$4}')%%\n)"
